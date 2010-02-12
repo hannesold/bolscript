8ff30b799a5e4d69fc293b47144f877e1f900523
hannes.oud <hannes.oud@282fc100-6ee3-11de-90d8-1fdda9445408>
2010-02-12 23:51:19 +0000

    History reading and writing seems to be working.

--- trunk/bolscript_src/bolscript/packets/HistoryEntries.java
+++ trunk/bolscript_src/bolscript/packets/HistoryEntries.java
@@ -7,9 +7,11 @@ import java.util.Date;
 
 import bolscript.packets.types.HistoryEntry;
 import bolscript.packets.types.HistoryEntryDateComparator;
+import bolscript.packets.types.HistoryOperationType;
 
 public class HistoryEntries implements BolscriptFormattableValue{
 	private ArrayList<HistoryEntry> entries;
+	private boolean changedSinceLastSorting = false;
 	
 	public HistoryEntries() {
 		entries = new ArrayList<HistoryEntry>();
@@ -17,7 +19,8 @@ public class HistoryEntries implements BolscriptFormattableValue{
 	
 	public void add(HistoryEntry entry) {
 		entries.add(entry);
-		
+		changedSinceLastSorting = true;
+		sort();
 	}
 	
 	public HistoryEntry get(int i) {
@@ -28,13 +31,18 @@ public class HistoryEntries implements BolscriptFormattableValue{
 		return entries.size();
 	}
 	
+	private void sort() {
+		if (!changedSinceLastSorting) {
+			Collections.sort(entries, HistoryEntryDateComparator.getStandard());
+			changedSinceLastSorting = true;
+		}
+	}
+	
 	/**
 	 * Returns the earliest Entries date, or null, if none was found
 	 * @return
 	 */
 	public HistoryEntry getCreationEntry() {
-		Collections.sort(entries, HistoryEntryDateComparator.getStandard());
-		
 		if (entries.size() <= 0) {
 			return null;
 		} else {
@@ -42,11 +50,35 @@ public class HistoryEntries implements BolscriptFormattableValue{
 		}
 	}
 
+	public HistoryEntry getLastModifiedEntry() {
+		if (entries.size() <= 0) {
+			return null;
+		} else {
+			HistoryEntry lastModificationEntry = null;
+			for (int i=0; i < entries.size(); i++) {
+				if (entries.get(i).getOperationTypeId() == HistoryOperationType.MODIFIED) {
+					lastModificationEntry = entries.get(i);
+				}
+			}
+			return lastModificationEntry;
+		}
+	}
+	
 	public String getCreationDateAsString() {
 		HistoryEntry creationEntry = getCreationEntry();
 		if (creationEntry != null) {
 			if (creationEntry.getDate() != null) {
 				return new SimpleDateFormat("yyyy.MM.dd").format(creationEntry.getDate());
+			}
+		}
+		return "";
+	}
+	
+	public String getModifiedDateAsString() {
+		HistoryEntry modifiedEntry = getLastModifiedEntry();
+		if (modifiedEntry != null) {
+			if (modifiedEntry.getDate() != null) {
+				return new SimpleDateFormat("yyyy.MM.dd").format(modifiedEntry.getDate());
 			}
 		}
 		return "";
@@ -70,4 +102,6 @@ public class HistoryEntries implements BolscriptFormattableValue{
 		return builder.toString();
 		
 	}
+
+
 }
